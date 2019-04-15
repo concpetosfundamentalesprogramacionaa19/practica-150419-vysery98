47ba168aa219f51aeca2184b1858352260f486d6
vysery98 <luispsi98@gmail.com>
2019-04-15 16:27:32 -0500

    python

---
+++ hola.py
@@ -0,0 +1,6 @@
+"""
+	Este es mi primer ejemplo
+	en lenguaje Python
+"""
+
+print ("Hola mundo UTPL")
---
+++ presentacion.py
@@ -0,0 +1,11 @@
+"""
+
+	Presentando en pantalla el nombre y apellido de una persona.
+
+"""
+
+	nombre="Fernando" 
+	apellido="Quizhpe"
+
+	print(nombre)
+	print(apellido)
